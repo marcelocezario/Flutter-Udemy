514107b7fb20cfa6f6df715c0d99d5c122b6fb7f
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-23 19:01:57 -0300

    Adicionando List View

--- lista_tarefas_app/lib/main.dart
+++ lista_tarefas_app/lib/main.dart
@@ -16,13 +16,44 @@ class ListaTarefasApp extends StatelessWidget {
 
 class ListaScreen extends StatelessWidget {
 
+  Widget getItem() {
+    return new Row(
+              children: <Widget>[
+                IconButton(
+                  icon: new Icon(Icons.check_box, color: Colors.green,),
+                  iconSize: 42.0,
+                  onPressed: () { },
+                ),
+                new Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text("Lavar o carro"),
+                    Text("13/09/2018")
+                  ],
+                )
+              ],
+            );
+  } 
+
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
         appBar: new AppBar(
           title: new Text("Lista de Tarefas")
         ),
-        body: Column()
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Expanded(child: 
+              ListView (
+                children: <Widget>[
+                  getItem()
+                ],
+              )
+            )
+          ],
+        )
     );
   }
 }
