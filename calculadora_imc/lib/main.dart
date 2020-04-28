d5f6968ac1e0a9c9106573e0b855148241d1caeb
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-28 10:24:36 -0300

    Adicionando Padding

--- calculadora_imc/lib/main.dart
+++ calculadora_imc/lib/main.dart
@@ -28,6 +28,7 @@ class _HomeState extends State<Home> {
         ),
         backgroundColor: Colors.white,
         body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10, 0.0, 10.0, 0.0),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
@@ -48,15 +49,18 @@ class _HomeState extends State<Home> {
                 textAlign: TextAlign.center,
                 style: TextStyle(color: Colors.green, fontSize: 25.0),
               ),
-              Container(
-                height: 50.0,
-                child: RaisedButton(
-                  onPressed: () {},
-                  child: Text(
-                    "Calcular",
-                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+              Padding(
+                padding: EdgeInsets.only(top: 10.0, bottom: 10.0),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: () {},
+                    child: Text(
+                      "Calcular",
+                      style: TextStyle(color: Colors.white, fontSize: 25.0),
+                    ),
+                    color: Colors.green,
                   ),
-                  color: Colors.green,
                 ),
               ),
               Text(
