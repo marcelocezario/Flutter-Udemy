999ac4b1c1e56c605e251432ef58b8d6d44e5b8c
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-29 14:46:14 -0300

    Apagando dados quandos campos estão vazios

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -41,21 +41,43 @@ class _HomeState extends State<Home> {
   double euro;
 
   void _realChanged(String text) {
-    double real = double.parse(text);
-    dolarController.text = (real/dolar).toStringAsFixed(2);
-    euroController.text = (real/euro).toStringAsFixed(2);
+    if (text.isEmpty) {
+      _clearAll();
+    } else {
+      double real = double.parse(text);
+      dolarController.text = (real * dolar).toStringAsFixed(2);
+      euroController.text = (real * euro).toStringAsFixed(2);
+    }
   }
 
   void _dolarChanged(String text) {
-    double dolar = double.parse(text);
-    realController.text = (dolar * this.dolar).toStringAsFixed(2);
-    euroController.text = (dolar * this.dolar / euro).toStringAsFixed(2);
+    if (text.isEmpty) {
+      _clearAll();
+    } else {
+      double dolar = double.parse(text);
+      double dolarEmReais = this.dolar;
+      double dolarEmEuro = this.dolar / this.euro;
+      realController.text = (dolar * dolarEmReais).toStringAsFixed(2);
+      euroController.text = (dolar * dolarEmEuro).toStringAsFixed(2);
+    }
   }
 
   void _euroChanged(String text) {
-    double euro = double.parse(text);
-    realController.text = (euro * this.euro).toStringAsFixed(2);
-    dolarController.text = (euro * this.euro / dolar).toStringAsFixed(2);
+    if (text.isEmpty) {
+      _clearAll();
+    } else {
+      double euro = double.parse(text);
+      double euroEmReais = this.euro;
+      double euroEmDolar = this.euro / this.dolar;
+      realController.text = (euro * euroEmReais).toStringAsFixed(2);
+      dolarController.text = (euro * euroEmDolar).toStringAsFixed(2);
+    }
+  }
+
+  void _clearAll() {
+    realController.text = "";
+    dolarController.text = "";
+    euroController.text = "";
   }
 
   @override
@@ -98,11 +120,14 @@ class _HomeState extends State<Home> {
                         children: <Widget>[
                           Icon(Icons.monetization_on,
                               size: 150.0, color: Colors.amber),
-                          buildTextField("Reais", "R\$", realController, _realChanged),
+                          buildTextField(
+                              "Reais", "R\$", realController, _realChanged),
                           Divider(),
-                          buildTextField("Dólares", "US\$", dolarController, _dolarChanged),
+                          buildTextField("Dólares", "US\$", dolarController,
+                              _dolarChanged),
                           Divider(),
-                          buildTextField("Euros", "€", euroController, _euroChanged),
+                          buildTextField(
+                              "Euros", "€", euroController, _euroChanged),
                         ],
                       ),
                     );
@@ -112,8 +137,8 @@ class _HomeState extends State<Home> {
   }
 }
 
-Widget buildTextField(
-    String label, String prefix, TextEditingController controller, Function function) {
+Widget buildTextField(String label, String prefix,
+    TextEditingController controller, Function function) {
   return TextField(
     controller: controller,
     decoration: InputDecoration(
