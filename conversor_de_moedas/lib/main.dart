e0ee9ce33d422ef3b3098d36ea0f6742bcc6e7ab
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-29 09:48:04 -0300

    Convertendo Json em map
    Convertando o Json retornado da API e convertendo ele em map para utilização das chaves primárias

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:async';
+import 'dart:convert';
 
 const request = "https://api.hgbrasil.com/finance?format=json-cors&key=50e5bb16";
 
 void main() async {
 
   http.Response response = await http.get(request);
-  print(response.body);
+  print(json.decode(response.body)["results"]["currencies"]["USD"]);
 
   runApp(MaterialApp(
     home: Container()
