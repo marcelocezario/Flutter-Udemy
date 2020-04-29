fe1e0aad60755f0db6171cbd00c37d9356d561d2
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-29 09:51:40 -0300

    Crianção função Futere<Map>
    Siginifica que a função vai retornar um valor futuro do tipo Map

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -7,10 +7,12 @@ const request = "https://api.hgbrasil.com/finance?format=json-cors&key=50e5bb16"
 
 void main() async {
 
-  http.Response response = await http.get(request);
-  print(json.decode(response.body)["results"]["currencies"]["USD"]);
-
   runApp(MaterialApp(
     home: Container()
   ));
+}
+
+Future<Map> getData() async {
+  http.Response response = await http.get(request);
+  return json.decode(response.body);
 }
