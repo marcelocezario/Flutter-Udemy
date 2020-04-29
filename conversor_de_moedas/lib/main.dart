370ef83f58826e11e6a023c2b9e46edc3e2271a9
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-28 23:12:51 -0300

    Utilizando API para busca de cotações

--- conversor_de_moedas/lib/main.dart
+++ conversor_de_moedas/lib/main.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
-const request = "https://api.hgbrasil.com/finance";
+const request = "https://api.hgbrasil.com/finance?format=json-cors&key=50e5bb16";
 
-void main(){
+void main() async {
+
+  http.Response response = await http.get(request);
+  print(response.body);
+
   runApp(MaterialApp(
     home: Container()
   ));
