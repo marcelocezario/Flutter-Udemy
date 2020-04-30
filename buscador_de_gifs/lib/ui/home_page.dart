a4a97610ad5da627e8f5f700ac5bc70774fa1ac1
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-30 08:39:12 -0300

    Fazendo requisição externa dos gifs

--- buscador_de_gifs/lib/ui/home_page.dart
+++ buscador_de_gifs/lib/ui/home_page.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+
+  String _search;
+  int _offset = 0;
+
+  _getGifs() async {
+    http.Response response;
+
+    if (_search == null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=rTFroV9LGWvq8V4NrfF4uqdq6p46U0wC&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=rTFroV9LGWvq8V4NrfF4uqdq6p46U0wC&q=dogs&limit=20&offset=75&rating=G&lang=en");
   }
 }
+
+@override
+Widget build(BuildContext context) {
+  return Container();
+}}
