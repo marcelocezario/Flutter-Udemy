9b376da699bada6f70acdc391db1045375a70e5b
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-04-30 08:27:20 -0300

    Criando nova pasta para elementos de interface

--- buscador_de_gifs/lib/main.dart
+++ buscador_de_gifs/lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:buscadordegifs/ui/home_page.dart';
 
 void main () {
   runApp(MaterialApp(
-
+    home: HomePage (
+      
+    ),
   ));
 }
