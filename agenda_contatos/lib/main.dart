af97c6054f8a4011679eb1cec9961f0e1f6d82a4
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-05-04 09:37:08 -0300

    Tirando banner de debug e chamando ContactPage para teste

--- agenda_contatos/lib/main.dart
+++ agenda_contatos/lib/main.dart
@@ -1,8 +1,12 @@
+import 'package:agendacontatos/ui/contact_page.dart';
 import 'package:agendacontatos/ui/home_page.dart';
 import 'package:flutter/material.dart';
 
+import 'helpers/contact_helper.dart';
+
 void main() {
   runApp(MaterialApp(
-    home: HomePage(),
+    home: ContactPage(),
+    debugShowCheckedModeBanner: false,
   ));
 }
