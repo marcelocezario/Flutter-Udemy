8df5a5b766495b2efb13f79ec93f133bc10cd420
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-05-04 18:37:08 -0300

    Passando telefone contato para ligação

--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 import 'package:agendacontatos/helpers/contact_helper.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import 'contact_page.dart';
 
@@ -117,7 +118,10 @@ class _HomePageState extends State<HomePage> {
                             "Ligar",
                             style: TextStyle(color: Colors.red, fontSize: 20.0),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            launch("tel:${contacts[index].phone}");
+                            Navigator.pop(context);
+                          },
                         ),
                       ),
                       Padding(
