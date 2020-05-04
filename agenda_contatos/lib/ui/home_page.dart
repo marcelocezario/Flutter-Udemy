e9974abd4d59b05ed471294adc95f69f8824a053
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-05-04 08:53:00 -0300

    Criando botão flutuante e atualizando lista de contatos ao abrir app

--- agenda_contatos/lib/ui/home_page.dart
+++ agenda_contatos/lib/ui/home_page.dart
@@ -10,26 +10,41 @@ class _HomePageState extends State<HomePage> {
 
   ContactHelper helper = ContactHelper();
 
+  List<Contct> contacts = List();
+
 
   @override
   void initState() {
     super.initState();
 
-    /*Contact c = Contact();
-    c.name = "Daniel Ciolfi";
-    c.email = "daniel@gmail.com";
-    c.phone = "534365436";
-    c.img = "imgtest";
-
-    helper.saveContact(c);*/
-
-    helper.getAllContacts().then((list) {
-      print(list);
+    helper.getAllContacts().then((list){
+      setState(() {
+        contacts = list;
+      });
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contatos"),
+        backgroundColor: Colors.red,
+        centerTitle: true,
+      ),
+      backgroundColor: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+        backgroundColor: Colors.red,
+      ),
+      body: ListView.builder(
+          padding: EdgeInsets.all(10.0),
+          itemCount: contacts.length,
+          itemBuilder: (context, index) {
+
+          }
+      ),
+    );
   }
 }
