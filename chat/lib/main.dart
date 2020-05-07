24e1b6efb3fb0f21830c7d9d67203e56d35ff65c
Marcelo Henrique Cezário <33532147+marcelocezario@users.noreply.github.com>
2020-05-07 18:10:37 -0300

    Enviando dados de mensagem para o Firebase

--- chat/lib/main.dart
+++ chat/lib/main.dart
@@ -4,18 +4,13 @@ import 'package:flutter/material.dart';
 
 void main() async{
   runApp(MyApp());
-
-  Firestore.instance.collection('mensagens').snapshots().listen((dado){
-    dado.documents.forEach((d){
-      print(d.data);
-    });
-  });
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
